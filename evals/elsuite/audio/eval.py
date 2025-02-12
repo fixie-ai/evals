021488ed60418f23043e9759fb983c8ea385985a
Patrick Li <patrick8289@gmail.com>
2025-02-12 11:25:24 -0800

    fix

--- evals/elsuite/audio/eval.py
+++ evals/elsuite/audio/eval.py
@@ -522,6 +522,7 @@ class BigBenchAudio(MatchAudioTask):
         return build_messages(self.DEFAULT_PROMPT, self.TASK_PROMPT, input)
 
     def _compute_metrics(self, sample: Sample, sampled: str):
+        print("question, generated_answer, expected_answer: ", sample["transcript"], sampled, sample["official_answer"])
         messages = [
             {"role": "system", "content": self.BIGBENCH_SYSTEM_PROMPT},
             {
@@ -613,7 +614,6 @@ class AudioBenchTask(MatchAudioTask):
         return build_messages(self.DEFAULT_PROMPT, " ".join(components), sample["audio"])
 
     def _compute_metrics(self, sample: Sample, sampled: str):
-        print("question, generated_answer, expected_answer: ", question, sampled, sample["answer"])
         prompt_template = self.AUDIOBENCH_USER_PROMPT_BINARY if self.is_mcq else self.AUDIOBENCH_USER_PROMPT
         question = sample["instruction"] + ("\n" + sample["choices"] if self.is_mcq else "")
         messages = [
